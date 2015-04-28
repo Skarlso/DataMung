f6a99218354b38c8443f283f2ba0c7a1154014b3
Gergely Brautigam <skarlso777@gmail.com>
2015-04-28 17:06:16 +0200

    Added second failing test. Let's see if Travis fails.

--- src/main/java/com/munging/DataMungingWeather.java
+++ src/main/java/com/munging/DataMungingWeather.java
@@ -1,11 +1,16 @@
 package com.munging;
 
 import java.net.URL;
+import java.util.List;
 
 public class DataMungingWeather {
 
     public URL readFileFromResources() {
         ClassLoader classLoader = getClass().getClassLoader();
         return classLoader.getResource("weather.dat");
+    }
+
+    public int doCompare(List<String> myTestList) {
+        return 0;
     }
 }
--- src/test/java/com/munging/DataMungingWeatherTest.java
+++ src/test/java/com/munging/DataMungingWeatherTest.java
@@ -4,6 +4,9 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
@@ -20,6 +23,11 @@ public class DataMungingWeatherTest {
     @Test
     public void testCanReadFileFromResources() throws Exception {
         Assert.assertThat(dataMungingWeather.readFileFromResources(), is(not(nullValue())));
+    }
 
+    @Test
+    public void testCanCompareAListOfSpacedNumbers() throws Exception {
+        List<String> myTestList = Arrays.asList("1 44 33", "2 99 98");
+        Assert.assertThat(dataMungingWeather.doCompare(myTestList), is(2));
     }
 }
